633a3cc6410163dfd1eca7f9e0aea7438dd2d3ca
RafikSameh <rafiksameh123@gmail.com>
2023-06-22 15:49:26 -0700

    free_stack r2

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * free_stack -  is a func to free stack
+ * @head: is a pointer
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *temp;
+	
+	temp = head;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+
