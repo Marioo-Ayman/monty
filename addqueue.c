bdacea9ac2059abe5af5f0a06506d5a99ee2d520
RafikSameh <rafiksameh123@gmail.com>
2023-06-22 16:36:25 -0700

    addqueue r3

---
+++ addqueue.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * addqueue - is a functions to add queue
+ * @head: is a pointer
+ * @n: is an integer
+ */
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new, *temp;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("ERROR\n");
+		exit(0);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+		new->prev = temp;
+	}
+}
