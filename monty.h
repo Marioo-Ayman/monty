9fe2fe69658cba8cbac4110f5677db04f0deb5a6
RafikSameh <rafiksameh123@gmail.com>
2023-06-24 15:47:29 -0700

    adv

--- monty.h
+++ monty.h
@@ -66,4 +66,5 @@ void addqueue(stack_t **head, int n);
 void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
 void f_sub(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
 #endif
